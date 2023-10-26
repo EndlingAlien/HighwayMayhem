67af8b4c981b5a414c1f9c839ed4aaa99623c6b9
EndlingAlien <alyssatarvin@gmail.com>
2023-10-26 17:10:27 -0600

    Created player controls
    Completely scraped original PlayerController script and updated it with new player input system. Created methods for rotation of turret, moving the tank on x axis (with smooth rotation to indicate direction), and basic starter method for firing the turret

--- Highway_Mayhem/Assets/Scripts/PlayerController.cs
+++ Highway_Mayhem/Assets/Scripts/PlayerController.cs
@@ -1,42 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //this is a variable
-    public float speed = 5.0f;
 
-    //turn the vehicle
-    public float turnSpeed = 100;
-    private float horizontalInput;
-    private float verticalInput;
+    [SerializeField] float playerSpeed = 5f;
+    [SerializeField] float lookSpeed = 5f;
+    [SerializeField] float rotationFactor = 1;
+    [SerializeField] Transform tankTurret;
+    float currentRotation;
+    float minRotation = -40;
+    float maxRotation = 40;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector2 moveInput;
+    Vector2 lookInput;
 
-    // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        //move vehicle forward
-        //super simple easy way, not very good tho
-        //transform.Translate(0, 0, 1);
-
-        //way better, but cant control speed
-        //transform.Translate(Vector3.forward);
-
-        //most effective, can control speed with time
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-
-        //turn the vehicle left and right
-        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-
-        //make the car rotate as you turn
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        MoveTurret();
+        MoveTank();
     }
+
+    void ShootTurret()
+    {
+        Debug.Log("shot fired");
+        //raycast?? and particle systems to indicate shot fired
+    }
+
+    #region Turret Control Methods
+        void MoveTurret()
+        {
+            float newRotation = currentRotation + lookInput.x * lookSpeed * Time.deltaTime;
+            SetCurrentRotation(newRotation);
+        }
+    
+        void SetCurrentRotation(float rot)
+        {
+            currentRotation = Mathf.Clamp(rot, minRotation, maxRotation);
+            tankTurret.transform.rotation = Quaternion.Euler(0, rot, 0);
+        }
+    #endregion
+
+    #region Tank Control Methods
+    void MoveTank()
+    {
+        Vector2 movement = ProcessMovement();
+
+        if (movement.x < 0)
+        {
+            ProcessRotation(-10);
+            ProcessMovement();
+        }
+        else if (movement.x > 0)
+        {
+            ProcessRotation(10);
+            ProcessMovement();
+        }
+        else if (movement.x == 0)
+        {
+            ProcessRotation(0);
+            ProcessMovement();
+        }
+    }
+
+    void ProcessRotation(float rotation)
+    {
+        Quaternion targetRotation = Quaternion.Euler(0, rotation, 0);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, rotationFactor);
+    }
+
+    Vector2 ProcessMovement()
+    {
+        Vector2 movement = new Vector2(moveInput.x * playerSpeed * Time.deltaTime, 0);
+
+        Vector3 newPosition = transform.position + new Vector3(movement.x, 0, 0);
+        transform.position = newPosition;
+        return movement;
+    }
+    #endregion
+
+    #region Input methods
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value)
+    {
+        ShootTurret();
+    }
+    #endregion
+
 }
